79a96e58f9c295ab261c624093035d4a2074314e
CarlosDuarte1998 <hdzalex1998@gmail.com>
2025-01-17 11:08:36 -0600

    commit initial

---
+++ resources/js/Layouts/AppClient.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Lenis from '@studio-freight/lenis';
+
+const lenis = new Lenis();
+
+lenis.on('scroll', (e) => {
+    // console.log(e);
+});
+
+function raf (time) {
+   lenis.raf(time)
+     requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf);
+
+
+</script>
+
+<template>
+    <main>
+        <slot />
+    </main>
+</template>
---
+++ resources/js/Pages/ClientView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AppClient from '@/Layouts/AppClient.vue';
+import HomeClient from './Client/HomeClient.vue';
+
+import { Head } from '@inertiajs/vue3';
+
+const title = 'Client View';
+
+
+
+</script>
+
+<template>
+    <main >
+        <AppClient>
+            <HomeClient>
+            </HomeClient>
+        </AppClient>
+    </main>
+</template>
+
+<style scoped>
+</style>
